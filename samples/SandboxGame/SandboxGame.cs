fdebb02f21481ad78a7b7552fd57204f0c8e6f18
PapaKodiak <brisendine.joseph92@gmail.com>
2026-06-05 11:35:01 -0500

    Split sandbox sample into scene files

---
+++ samples/SandboxGame/SandboxGame.cs
@@ -0,0 +1,21 @@
+﻿using Sindri.Core;
+
+internal sealed class SandboxGame : SindriGame
+{
+    public override string Name => "Sindri Sandbox";
+
+    public override void Configure(EngineConfig config)
+    {
+        config.WindowTitle = Name;
+        config.TargetWidth = 1280;
+        config.TargetHeight = 720;
+        config.LimitFrameRate = true;
+        config.TargetFramesPerSecond = 60;
+        config.ShowFrameRateInTitle = true;
+    }
+
+    public override IScene CreateInitialScene()
+    {
+        return new SandboxScene();
+    }
+}
